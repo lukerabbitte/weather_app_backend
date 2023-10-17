52d53ab5436cf5abf738baf0e897889b93c14037
lukerabbitte <luke.rabbitte@gmail.com>
2023-10-17 22:41:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
+
+const app = express();
+const PORT = 8080;
+const API_KEY = 'e689a22a0aa07bb6cfa7018f8db86a92';
+const OPENWEATHERMAP_GEOCODING_API = 'http://api.openweathermap.org/geo/1.0/direct';
+
+app.use(express.json())
+
+app.use(cors({
+    //origin: 'http://your-frontend-server.com',
+    //optionsSuccessStatus: 200
+}));
+
+app.listen(
+    PORT,
+    () => console.log(`Server active on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'green crewneck',
+        size: "large"
+    });
+});
+
+// Get coordinates based on city name
+app.get('/geocoding', async (req, res) => {
+    const { cityName, countryCode, limit } = req.query;
+    /*const cityName = "Dublin";
+    const countryCode = "IE";
+    const limit = 1; */
+    try {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=${limit}&appid=e689a22a0aa07bb6cfa7018f8db86a92`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching weather data.' });
+    }
+});
+
+app.get('/weather', async (req, res) => {
+    const { lat, lon } = req.query;
+    try {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=${limit}&appid=e689a22a0aa07bb6cfa7018f8db86a92`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching weather data.' });
+    }
+});
