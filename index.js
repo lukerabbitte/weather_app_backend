927697a09f7d36a230c1962906db83b71069af5e
lukerabbitte <luke.rabbitte@gmail.com>
2023-10-20 09:02:41 +0100

    Unsplash only requesting landscape pics

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
-const API_KEY = 'e689a22a0aa07bb6cfa7018f8db86a92';
-const OPENWEATHERMAP_GEOCODING_API = 'http://api.openweathermap.org/geo/1.0/direct';
 
 app.use(express.json())
 
@@ -41,7 +39,7 @@ app.get('/geocoding', async (req, res) => {
 app.get('/weather', async (req, res) => {
     const { lat, lon, lang } = req.query;
     try {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e689a22a0aa07bb6cfa7018f8db86a92&units=metric&lang=${lang}&cnt=4`); // TODO add UI for changing units
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e689a22a0aa07bb6cfa7018f8db86a92&units=metric&lang=${lang}&cnt=40`); // TODO add UI for changing units
         const data = await response.json();
         res.json(data);
     } catch (error) {
@@ -60,4 +58,15 @@ app.get('/air_pollution', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching air pollution data.' });
     }
 });
+
+app.get('/unsplash_photos', async (req, res) => {
+    const { city } = req.query;
+    try {
+        const response = await fetch(`https://api.unsplash.com/search/photos/?client_id=r_wUIHMUiHEOd0pCFkr_VoV2VfxNQY2ID8x4Attz7d4&page=1&orientation=landscape&query=${city} city`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while fetching unsplash data.' });
+    }
+})
 
